c0e0307b24e2db827f5632d56dcf44194d97d6a5
Miriam Wagner <miriam.wagner@rwth-aachen.de>
2019-08-11 15:40:44 +0200

    MFS with events running

--- version 1/Algorithm.py
+++ version 1/Algorithm.py
@@ -1,12 +1,11 @@
 from pm4py.objects.log.importer.xes import factory as xes_import_factory
 import simplifyDeleteTracesStand5
 import mvsBoxplot
-import PatternMFSEvents
+import PatternMFS
 import time
 import TrajectoryDataAnonymizer
 #import log (parameters={"max_no_traces_to_import": n} to have a faster workflow), specify sensitive values and how time should be handled
 log = xes_import_factory.apply("Sepsis Cases - Event Log.xes")#, parameters={"max_no_traces_to_import": 1000})
-
 sensitive = ['Age', 'Diagnose']
 spectime = "minutes"
 cont = ['Age']
@@ -17,18 +16,16 @@ start = time.time()
 #T are all traces
 logsimple, T = simplifyDeleteTracesStand5.simplify(log, sensitive, spectime)
 logtime = time.time()
-print("simplify:", logtime-start)
+print(logtime-start)
 L = 2
 K = 5
 C = 0.5
 # Output: Minimal violating sequence V (T )
 violating = mvsBoxplot.mvs(T, L, K, C, sensitive, logsimple, cont)
-frequent = PatternMFSEvents.mfs(T, K)
-mvstime = time.time()
-print("MVS and MFS:", mvstime-logtime)
+frequent = PatternMFS.mfs(T, K)
+
 sup = TrajectoryDataAnonymizer.suppression(violating, frequent)
-suptime = time.time()
-print("suppresion", suptime-mvstime)
+
 T_ = TrajectoryDataAnonymizer.suppressT(logsimple, sup)
-anonymizertime = time.time()
-print(anonymizertime-suptime)
+mvstime = time.time()
+print(mvstime-logtime)
