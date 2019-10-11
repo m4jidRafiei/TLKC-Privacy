1ffab7c73c663613609f6ab4349f3dd5d7fc97cb
Miriam Wagner <miriam.wagner@rwth-aachen.de>
2019-10-11 10:09:21 +0200

    baseline

--- baseline_k-annonymity.py
+++ baseline_k-annonymity.py
@@ -4,18 +4,22 @@ from pm4py.statistics.traces.tracelog import case_statistics
 from pm4py.algo.filtering.tracelog.variants import variants_filter
 event_log = "Sepsis Cases - Event Log.xes"
 
-K2 = [0.7, 0.8, 0.9]
-for k in K2:
+K = [20, 40, 80, 160]
+for k in K:
     log = xes_import_factory.apply(event_log)
     var_with_count = case_statistics.get_variant_statistics(log)
-    variants_count = sorted(var_with_count, key=lambda x: x['count'], reverse=False)
+    variants_count = sorted(var_with_count, key=lambda x: x['count'], reverse=True)
     to_filter = []
-    print(variants_count)
+    count = 0
     for j in range(0, len(variants_count)):
         dict = variants_count[j]
-        if dict["count"] < k*len(log):
+        if dict["count"] < k:
             to_filter.append([dict["variant"]])
-    for fil in to_filter:
-        log = variants_filter.apply(log, fil)
+        else:
+            count += dict["count"]
+    print(count)
+    for delete in to_filter:
+        log = variants_filter.apply(log, delete,parameters={"positive": False})
+    len(log)
     xes_exporter.export_log(log, "xes/baseline" + "_" + str(k) + "-" + "Annonymity" + ".xes")
     print("xes/baseline" + "_" + str(k) + "-" + "Annonymity" + ".xes" + " has been exported!")
