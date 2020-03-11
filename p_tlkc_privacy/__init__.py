736d524a7b10fde60dea13efaef6d68e62cc668d
Majid <m4jid.rafiei@gmail.com>
2020-03-11 16:20:42 +0100

    privacy metadata integrated

--- p_tlkc_privacy/__init__.py
+++ p_tlkc_privacy/__init__.py
@@ -1,7 +1,7 @@
 from p_tlkc_privacy import Anonymizer, ELRepresentation, MFS, MVS, privacyPreserving, results
 
 __name__ = 'p_tlkc_privacy'
-__version__ = '0.0.3'
+__version__ = '0.0.4'
 __doc__ = "TLKC-privacy model for process mining"
 __author__ = 'Majid Rafiei'
 __author_email__ = 'majid.rafiei@pads.rwth-aachen.de'
--- p_tlkc_privacy/privacyPreserving.py
+++ p_tlkc_privacy/privacyPreserving.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 from pm4py.objects.log.importer.xes import factory as xes_importer_factory
 from pm4py.objects.log.exporter.xes import factory as xes_exporter
+from p_privacy_metadata.privacyExtension import privacyExtension
 from p_tlkc_privacy import Anonymizer
 import os
 
@@ -50,6 +51,9 @@ class privacyPreserving(object):
                                 dict1 = dict2
                                 for t in T:
                                     privacy_aware_log_path = os.path.join(directory," " +fixed_name + "set" + "_" + str(l) + "_" + str(k) + "_" + str(c) + "_" + str(k2) + "_" + t + ".xes")
+                                    # add privacy metadata
+                                    self.add_privacy_metadata(log_set[t])
+
                                     xes_exporter.export_log(log_set[t],privacy_aware_log_path)
                                     print(fixed_name + "set" + "_" + str(l) + "_" + str(k) + "_" + str(c) + "_" + str(
                                         k2) + "_" + t + ".xes" + " has been exported!")
@@ -63,6 +67,9 @@ class privacyPreserving(object):
                                 for t in T:
                                     privacy_aware_log_path = os.path.join(directory, fixed_name + "multiset" + "_" + str(l) + "_" + str(
                                         k) + "_" + str(c) + "_" + str(k2) + "_" + t + ".xes")
+                                    # add privacy metadata
+                                    self.add_privacy_metadata(log_set_count[t])
+
                                     xes_exporter.export_log(log_set_count[t], privacy_aware_log_path)
                                     print(fixed_name + "multiset" + "_" + str(l) + "_" + str(k) + "_" + str(c) + "_" + str(
                                         k2) + "_" + t + ".xes" + " has been exported!")
@@ -77,6 +84,9 @@ class privacyPreserving(object):
                                 for t in T:
                                     privacy_aware_log_path = os.path.join(directory, fixed_name + "sequence" + "_" + str(l) + "_" + str(
                                         k) + "_" + str(c) + "_" + str(k2) + "_" + t + ".xes")
+                                    # add privacy metadata
+                                    self.add_privacy_metadata(log_seq_count[t])
+
                                     xes_exporter.export_log(log_seq_count[t], privacy_aware_log_path)
                                     print(fixed_name + "sequence" + "_" + str(l) + "_" + str(k) + "_" + str(c) + "_" + str(
                                         k2) + "_" + t + ".xes" + " has been exported!")
@@ -86,9 +96,20 @@ class privacyPreserving(object):
                                         anonymizer.seq_time(self.log, sensitive, cont, t, l, k, c, k2, dict1)
                                     privacy_aware_log_path = os.path.join(directory, fixed_name + "relative" + "_" + str(l) + "_" + str(
                                         k) + "_" + str(c) + "_" + str(k2) + "_" + t + ".xes")
+                                    #add privacy metadata
+                                    self.add_privacy_metadata(log_time)
+
                                     xes_exporter.export_log(log_time, privacy_aware_log_path)
                                     print(fixed_name + "relative" + "_" + str(l) + "_" + str(k) + "_" + str(c) + "_" + str(
                                         k2) + "_" + t + ".xes" + " has been exported!")
                                     dict1 = dict2
                         except Exception as e:
                             print(e)
+
+    def add_privacy_metadata(self,log):
+        prefix = 'privacy:'
+        uri = 'paper_version_uri/privacy.xesext'
+
+        privacy = privacyExtension(log, prefix, uri)
+        privacy.set_privacy_tracking('suppression', 'event', 'event')
+
